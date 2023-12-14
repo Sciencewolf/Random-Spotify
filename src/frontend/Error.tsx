5ae0a8fe70525fd002c3c5288afca0acb751da1a
Sciencewolf <aron17marton@gmail.com>
2023-12-14 21:17:31 +0100

    removed unnecessary files

--- src/frontend/Error.tsx
+++
@@ -1,9 +0,0 @@
-export default function Error(props: {message: string}) {
-    return (
-        <>
-            <h1>
-                Error: {props.message}
-            </h1>
-        </>
-    )
-}
