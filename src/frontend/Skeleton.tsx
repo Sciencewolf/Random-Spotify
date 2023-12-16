50a8069c3918202eb3884cc9439a26825ab37cf3
Sciencewolf <aron17marton@gmail.com>
2023-12-17 00:17:59 +0100

    removed onClick for <Main /> component, styling

--- src/frontend/Skeleton.tsx
+++ src/frontend/Skeleton.tsx
@@ -1,21 +1,12 @@
 import '../style/Skeleton.css'
-import {useState} from "react";
-import Main from "../backend/Main.tsx";
 
-function Skeleton(props: {imgSRC: string, songName: string, songArtist: string}) {
-    const [isClicked, setIsClicked] = useState(false)
-
-    function handleClick() {
-        setIsClicked(true)
-    }
-
+function Skeleton() {
     return (
         <>
             <div className={"skeleton-div"}
                  id={"skeleton-div"}>
-                <img src={props.imgSRC}
+                <div
                      id={"song-img"}
-                     alt={"song-img"}
                 />
                 <h2 className={"songName-h2"}
                     id={"songName-h2"}
@@ -25,7 +16,6 @@ function Skeleton(props: {imgSRC: string, songName: string, songArtist: string})
                 />
                 <button className={"play-button"}
                         id={"play-button"}
-                        onClick={() => handleClick()}
                 >
                     <img src={"https://img.icons8.com/windows/32/play--v1.png"}
                          alt={"play-btn"}
@@ -33,8 +23,6 @@ function Skeleton(props: {imgSRC: string, songName: string, songArtist: string})
                     />
                 </button>
             </div>
-
-            {isClicked ? <Main/> : ""}
         </>
     )
 }
