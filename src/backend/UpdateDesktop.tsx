3a3cf7f551ee5cb4739066452d73144c46868bf5
Sciencewolf <aron17marton@gmail.com>
2024-01-17 20:49:04 +0100

    chore: currently removed info about playlist and artist

--- src/backend/UpdateDesktop.tsx
+++ src/backend/UpdateDesktop.tsx
@@ -1,28 +1,29 @@
-import AboutTheArtist from "../frontend/AboutTheArtist.tsx";
+// import AboutTheArtist from "../frontend/AboutTheArtist.tsx";
 import UpdateSkeleton from "../frontend/UpdateSkeleton.tsx";
-import ShowPlaylistOfSong from "../frontend/ShowPlaylistOfSong.tsx";
+// import ShowPlaylistOfSong from "../frontend/ShowPlaylistOfSong.tsx";
 import UpdateTitle from "../frontend/UpdateTitle.tsx";
 import {ArtistProps, PlaylistProps, Props, SongProps} from "./Props.ts";
 import Footer from "../frontend/Footer.tsx";
 
-function updateDesktop({artistName, artistImg, followersArtist,
+function updateDesktop({
+                           // artistName, artistImg, followersArtist,
                        songImg, songName, songArtist,
-                       playlistImg, playlistName, followersPlaylist,
+                       // playlistImg, playlistName, followersPlaylist,
                        title}: ArtistProps & SongProps & PlaylistProps & Props) {
     return (
         <>
-            <AboutTheArtist artistName={artistName}
-                            artistImg={artistImg}
-                            followersArtist={followersArtist + " followers"}
-            />
+            {/*<AboutTheArtist artistName={artistName}*/}
+            {/*                artistImg={artistImg}*/}
+            {/*                followersArtist={followersArtist + " followers"}*/}
+            {/*/>*/}
             <UpdateSkeleton songImg={songImg}
                             songName={songName}
                             songArtist={songArtist}
             />
-            <ShowPlaylistOfSong playlistImg={playlistImg}
-                                playlistName={playlistName}
-                                followersPlaylist={followersPlaylist}
-            />
+            {/*<ShowPlaylistOfSong playlistImg={playlistImg}*/}
+            {/*                    playlistName={playlistName}*/}
+            {/*                    followersPlaylist={followersPlaylist}*/}
+            {/*/>*/}
             <UpdateTitle title={title} />
             <Footer isChangeFooterClassName={true}/>
         </>
--- src/backend/UpdateMobile.tsx
+++ src/backend/UpdateMobile.tsx
@@ -1,26 +1,29 @@
-import ShowPlaylistOnMobile from "../frontend/ShowPlaylistOnMobile.tsx";
+// import ShowPlaylistOnMobile from "../frontend/ShowPlaylistOnMobile.tsx";
 import UpdateSkeleton from "../frontend/UpdateSkeleton.tsx";
-import AboutTheArtist from "../frontend/AboutTheArtist.tsx";
+// import AboutTheArtist from "../frontend/AboutTheArtist.tsx";
 import Footer from "../frontend/Footer.tsx";
 import {ArtistProps, PlaylistProps, Props, SongProps} from "./Props.ts";
 
-function updateMobile({playlistName, songImg,
+function updateMobile({
+                          // playlistName,
+                          songImg,
                           songName, songArtist,
-                          artistImg, followersArtist, token
+                          // artistImg, followersArtist,
+                          token
                       }: PlaylistProps & SongProps & ArtistProps & Props) {
 
     return (
         <>
-            <ShowPlaylistOnMobile playlistName={playlistName}/>
+            {/*<ShowPlaylistOnMobile playlistName={playlistName}/>*/}
             <UpdateSkeleton songImg={songImg}
                             songName={songName}
                             songArtist={songArtist}
                             token={token}
             />
-            <AboutTheArtist artistName={songArtist}
-                            artistImg={artistImg}
-                            followersArtist={followersArtist + " followers"}
-            />
+            {/*<AboutTheArtist artistName={songArtist}*/}
+            {/*                artistImg={artistImg}*/}
+            {/*                followersArtist={followersArtist + " followers"}*/}
+            {/*/>*/}
             <Footer isChangeFooterClassName={true}/>
         </>
     )
